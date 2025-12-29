f7c5c6919c89d60dfb61005d15e0fedbd26b4692
Jeff Kern <jeff.kern@gmail.com>
2025-12-29 13:01:16 +0200

    stabilize Next dev server

--- next.config.ts
+++ next.config.ts
@@ -5,6 +5,9 @@ const nextConfig: NextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
   },
+  experimental: {
+    webpackBuildWorker: false,
+  },
   webpack: (config, { isServer }) => {
     if (isServer) {
       config.output = config.output ?? {};
